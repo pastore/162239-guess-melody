02623710666a08ae5a3252809ea1dca483ce6a66
pastore <pastorednepr@gmail.com>
2017-05-27 14:17:17 +0300

    format code by eslint style

--- js/main.js
+++ js/main.js
@@ -1,38 +1,35 @@
-﻿'use strict';
+﻿const template = document.querySelector(`#templates`);
+const screens = template.content.querySelectorAll(`.main`);
+const sectionMain = document.querySelector(`section.main`);
+const arrowKeys = {Left: 37, Right: 39};
+let currentScreen = 4;
+const showScreen = (numberScreen = currentScreen) => {
+  const parse = parseInt(numberScreen, 10) || 4;
+  if (parse >= 0 && parse < screens.length) {
+    sectionMain.innerHTML = screens[parse].innerHTML;
+  }
+};
 
-let template = document.querySelector("#templates"),
-    screens = template.content.querySelectorAll(".main"),
-    sectionMain = document.querySelector("section.main"),
-    arrowKeys = { Left: 37, Right: 39 },
-    currentScreen = 4,
-    showScreen = (numberScreen = currentScreen) => {
-        var parse = parseInt(numberScreen) || 4;
-        if (parse >= 0 && parse < screens.length) {
-            sectionMain.innerHTML = screens[parse].innerHTML;
+document.addEventListener(`keyup`, function (e) {
+  const keyCode = e.keyCode;
+  if (e.altKey) {
+    switch (keyCode) {
+      case arrowKeys.Left:
+        --currentScreen;
+        if (currentScreen === -1) {
+          currentScreen = screens.length - 1;
         }
-    };
-
-document.addEventListener("keyup", function (e) {
-    let keyCode = e.keyCode;
-
-    if (e.altKey){
-        switch (keyCode){
-            case arrowKeys.Left:
-                --currentScreen;
-                if (currentScreen == -1){
-                    currentScreen = screens.length - 1;;
-                }
-                showScreen();
-                break;
-            case arrowKeys.Right:
-                ++currentScreen;
-                if (currentScreen == screens.length) {
-                    currentScreen = 0;
-                }
-                showScreen();
-                break;
+        showScreen();
+        break;
+      case arrowKeys.Right:
+        ++currentScreen;
+        if (currentScreen === screens.length) {
+          currentScreen = 0;
         }
+        showScreen();
+        break;
     }
+  }
 });
 
 showScreen();
