7a5af58474e0a133df1356e3e52bf5ea52637fef
pastore <pastorednepr@gmail.com>
2017-05-29 10:26:23 +0300

    fix format style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-﻿const template = document.querySelector(`#templates`);
+const template = document.querySelector(`#templates`);
 const screens = template.content.querySelectorAll(`.main`);
 const sectionMain = document.querySelector(`section.main`);
-const arrowKeys = { Left: 37, Right: 39 };
+const arrowKeys = {Left: 37, Right: 39};
 const SCREEN_WELCOME_INDEX = 4;
 let currentScreen = SCREEN_WELCOME_INDEX;
 const showScreen = (numberScreen = currentScreen) => {
-    const parse = parseInt(numberScreen, 10) || SCREEN_WELCOME_INDEX;
+  const parse = parseInt(numberScreen, 10) || SCREEN_WELCOME_INDEX;
   if (parse >= 0 && parse < screens.length) {
     sectionMain.innerHTML = screens[parse].innerHTML;
   }
