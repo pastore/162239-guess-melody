c1a8a66f7964566bd454e9cd3f84a66a15e49ae1
pastore <pastorednepr@gmail.com>
2017-05-29 16:30:44 +0300

    fix to show screen with index zero and ordered display screens

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 const template = document.querySelector(`#templates`);
-const screens = template.content.querySelectorAll(`.main`);
+let screens = template.content.querySelectorAll(`.main`);
+const arrayOrders = [4, 3, 0, 1, 2];
+let orderedScreens = [];
+[...screens].forEach((screen, indexScreen) => {
+  const tempIndex = arrayOrders.indexOf(indexScreen);
+  orderedScreens[tempIndex] = screen;
+});
 const sectionMain = document.querySelector(`section.main`);
 const arrowKeys = {Left: 37, Right: 39};
-const SCREEN_WELCOME_INDEX = 4;
+const SCREEN_WELCOME_INDEX = 0;
 let currentScreen = SCREEN_WELCOME_INDEX;
 const showScreen = (numberScreen = currentScreen) => {
-  const parse = parseInt(numberScreen, 10) || SCREEN_WELCOME_INDEX;
-  if (parse >= 0 && parse < screens.length) {
-    sectionMain.innerHTML = screens[parse].innerHTML;
+  const parse = isNaN(parseInt(numberScreen, 10)) ? SCREEN_WELCOME_INDEX : parseInt(numberScreen, 10);
+  if (parse >= 0 && parse < orderedScreens.length) {
+    sectionMain.innerHTML = orderedScreens[parse].innerHTML;
   }
 };
 
@@ -16,15 +22,15 @@ document.addEventListener(`keyup`, function (e) {
   if (e.altKey) {
     switch (keyCode) {
       case arrowKeys.Left:
-        --currentScreen;
+        currentScreen--;
         if (currentScreen === -1) {
-          currentScreen = screens.length - 1;
+          currentScreen = orderedScreens.length - 1;
         }
         showScreen();
         break;
       case arrowKeys.Right:
-        ++currentScreen;
-        if (currentScreen === screens.length) {
+        currentScreen++;
+        if (currentScreen === orderedScreens.length) {
           currentScreen = 0;
         }
         showScreen();
