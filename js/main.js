947611f71cf928143a4f05f4386576ad67176317
pastore <pastorednepr@gmail.com>
2017-05-29 10:20:54 +0300

    add const variable for screen welcome

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 ﻿const template = document.querySelector(`#templates`);
 const screens = template.content.querySelectorAll(`.main`);
 const sectionMain = document.querySelector(`section.main`);
-const arrowKeys = {Left: 37, Right: 39};
-let currentScreen = 4;
+const arrowKeys = { Left: 37, Right: 39 };
+const SCREEN_WELCOME_INDEX = 4;
+let currentScreen = SCREEN_WELCOME_INDEX;
 const showScreen = (numberScreen = currentScreen) => {
-  const parse = parseInt(numberScreen, 10) || 4;
+    const parse = parseInt(numberScreen, 10) || SCREEN_WELCOME_INDEX;
   if (parse >= 0 && parse < screens.length) {
     sectionMain.innerHTML = screens[parse].innerHTML;
   }
