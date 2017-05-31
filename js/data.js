4970c7dc3e32a830cd39ad2512a7bb11cebf5792
pastore <pastorednepr@gmail.com>
2017-06-01 01:16:21 +0300

    add 7 audio files start to add structure data

---
+++ js/data.js
@@ -0,0 +1,18 @@
+﻿import audioList from './audioList';
+
+const intialState = {
+    level: `level-artist`,
+    lives: 3,
+    time: 0
+};
+
+const levels = {
+    'level-artist': {
+        artist: {},
+        answers: []
+    },
+    'level-genre': {
+        genre: {},
+        answers: []
+    }
+};
