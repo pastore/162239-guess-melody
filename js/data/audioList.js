8f58d8e11b4d54c40904d2dfd4e81656aada78b8
pastore <pastorednepr@gmail.com>
2017-06-13 19:34:46 +0300

    change audio paths

--- js/data/audioList.js
+++ js/data/audioList.js
@@ -2,49 +2,49 @@ const audioList = [
   {
     id: 0,
     name: `The Big Guns`,
-    path: `../../audio/The_Big_Guns.mp3`,
+    path: `./audio/The_Big_Guns.mp3`,
     author: `Silent Partner`,
     genre: `Rock`
   },
   {
     id: 1,
     name: `Behind Closed Doors`,
-    path: `../../audio/Behind_Closed_Doors.mp3`,
+    path: `./audio/Behind_Closed_Doors.mp3`,
     author: `Otis McDonald`,
     genre: `Hip-hop`
   },
   {
     id: 2,
     name: `Volcano Trap`,
-    path: `../../audio/Volcano_Trap.mp3`,
+    path: `./audio/Volcano_Trap.mp3`,
     author: `Gunar Olsen`,
     genre: `Hip-hop`
   },
   {
     id: 3,
     name: `Oh, My Love`,
-    path: `../../audio/Oh_My_Love.mp3`,
+    path: `./audio/Oh_My_Love.mp3`,
     author: `Ever Almond`,
     genre: `Country`
   },
   {
     id: 4,
     name: `Safary time`,
-    path: `../../audio/Safari_Time.mp3`,
+    path: `./audio/Safari_Time.mp3`,
     author: `Jingle Punks`,
     genre: `Country`
   },
   {
     id: 5,
     name: `Bumper Tag`,
-    path: `../../audio/Bumper_Tag.mp3`,
+    path: `./audio/Bumper_Tag.mp3`,
     author: `John Deley`,
     genre: `Jazz`
   },
   {
     id: 6,
     name: `Bitters At The Saloon`,
-    path: `../../audio/Bitters_At_The_Saloon.mp3`,
+    path: `./audio/Bitters_At_The_Saloon.mp3`,
     author: `Bird Creek`,
     genre: `Jazz`
   }
